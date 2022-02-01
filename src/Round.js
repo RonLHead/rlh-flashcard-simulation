441245a96eb1bb41ff55a2fb8941509db0f5a52a
Ronald Head <ronaldlhead@gmail.com>
2022-02-01 15:04:49 -0700

    Pass assertion currentCard being first card in deck

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,7 @@
 class Round {
   constructor(deck) {
-    this.deck = deck; 
+    this.deck = deck;
+    this.currentCard = deck.cards[0];
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -8,12 +8,20 @@ const Round = require('../src/Round');
 describe('Round', function() {
 
   it('should be a function', function() {
-    const round = new Round();
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
     expect(Round).to.be.a('function');
   });
 
   it('should be an instance of Round', function() {
-    const round = new Round();
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
     expect(round).to.be.an.instanceof(Round);
   });
 
@@ -23,6 +31,15 @@ describe('Round', function() {
     const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
-    expect(round.deck).to.equal(deck)
+    expect(round.deck).to.equal(deck);
   });
+
+  it('should have the first Card of the Deck be the current Card', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    expect(round.currentCard).to.equal(card1);
+  })
 });
