7f27c71f007987bfc2afb7f3a64a2eb1b08cfce4
Ronald Head <ronaldlhead@gmail.com>
2022-01-31 20:42:08 -0700

    Pass return user guess test

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -16,9 +16,15 @@ describe('Turn', function() {
     expect(turn).to.be.an.instanceof(Turn);
   });
 
-  it('should take a user\s guess to the question', function() {
+  it("should take a user's guess to the question", function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('object', card);
     expect(turn.guess).to.equal('object');
+  });
+
+  it("should return the user's guess", function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    expect(turn.returnGuess()).to.equal('object');
   });
 });
