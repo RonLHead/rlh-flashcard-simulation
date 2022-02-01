8e1bb9c89635bfe00d77d95b04278674c6a180e9
Ronald Head <ronaldlhead@gmail.com>
2022-01-31 22:56:59 -0700

    Initalize Deck object test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -10,4 +10,10 @@ describe('Deck', function() {
     const deck = new Deck();
     expect(Deck).to.be.a('function');
   });
+
+  it('should be an instance of Deck', function() {
+    const deck = new Deck();
+    expect(deck).to.be.an.instanceof(Deck);
+  });
+
 });
