bc1f8b74ce72c2ba713f6793b91b149361b40791
Ronald Head <ronaldlhead@gmail.com>
2022-02-01 15:22:31 -0700

    Pass assertion returning current card

--- test/Round-test.js
+++ test/Round-test.js
@@ -60,4 +60,13 @@ describe('Round', function() {
     const round = new Round(deck);
     expect(round.incorrectGuesses).to.deep.equal([]);
   });
+
+  it('should return current Card being played', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    expect(round.returnCurrentCard()).to.equal(card1)
+  });
 });
