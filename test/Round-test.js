704543b2f18208a5ad339bc4a67adae2cd7b3f61
Ronald Head <ronaldlhead@gmail.com>
2022-02-01 15:44:05 -0700

    Pass assertion evaluating incorrect guess

--- test/Round-test.js
+++ test/Round-test.js
@@ -90,4 +90,14 @@ describe('Round', function() {
     expect(round.takeTurn('sea otter')).to.equal('correct!');
     expect(round.turns).to.equal(1);
   });
+
+  it('should evalulate an incorrect guess', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    expect(round.takeTurn('pug')).to.equal('incorrect!');
+    expect(round.turns).to.equal(1);
+  });
 });
