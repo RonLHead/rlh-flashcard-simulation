6cfb93132e21096e3e9f2641615792d5e06bf3ec
Ronald Head <ronaldlhead@gmail.com>
2022-02-01 19:21:29 -0700

    Pass assertion for function

---
+++ test/Game-test.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Game = require('../src/Game');
+
+describe('Game', function() {
+
+  it('should be a function', function() {
+    const game = new Game();
+    expect(Game).to.be.a('function');
+  });
+
+  
+});
