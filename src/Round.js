f2d35940426b208a025c30eec2d2e17e0def09f7
Ronald Head <ronaldlhead@gmail.com>
2022-02-07 16:39:02 -0700

    Correct Round class & test

--- src/Round.js
+++ src/Round.js
@@ -3,7 +3,7 @@ const Turn = require('./Turn')
 class Round {
   constructor(deck) {
     this.deck = deck;
-    this.currentCard = this.returnCurrentCard();
+    this.currentCard = this.deck.cards[0];
     this.turns = 0;
     this.incorrectGuesses = [];
   }
